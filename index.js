c31d2ee9aa32338a9cc5f49f937112488e71d893
karthi2499 <82304939+karthi2499@users.noreply.github.com>
2022-07-04 19:36:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
     await mongoose.connect(
-        "mongodb://karthi:karthi@project.provendb.io/project?ssl=true", {
+        "", {
             useUnifiedTopology: true,
             useNewUrlParser: true
         }
@@ -68,7 +68,7 @@ app.post("/ElectrictyComplaints", async (req, res) => {
     let tree = builder.build();
 
     // Creating a New Anchor Client
-    let client = anchor.connect(anchor.withCredentials("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhbmNob3IiLCJleHAiOjE3OTMyNzA0NDUsImp0aSI6ImFvNng3NWo1ZWVrcm0yNnpzY2sxeXdiaSIsInN1YiI6InV1aWd3Yzd6YXhlanJxZHNnYXF6M3NidSIsInNjb3BlIjoiMzEiLCJyb2xlIjoiRnJlZSJ9.Z--sFCzSKffue0cW4eyI4EuEM93R8bP8KRRUzqkGkhc"));
+    let client = anchor.connect(anchor.withCredentials(""));
 
     // Submitting Proof
     let proof = await client.submitProof(tree.getRoot(), 
@@ -137,7 +137,7 @@ app.post("/PollutionComplaints", async (req, res) => {
     let tree = builder.build();
 
     // Creating a New Anchor Client
-    let client = anchor.connect(anchor.withCredentials("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhbmNob3IiLCJleHAiOjE3OTMyNzA0NDUsImp0aSI6ImFvNng3NWo1ZWVrcm0yNnpzY2sxeXdiaSIsInN1YiI6InV1aWd3Yzd6YXhlanJxZHNnYXF6M3NidSIsInNjb3BlIjoiMzEiLCJyb2xlIjoiRnJlZSJ9.Z--sFCzSKffue0cW4eyI4EuEM93R8bP8KRRUzqkGkhc"));
+    let client = anchor.connect(anchor.withCredentials(""));
 
     // Submitting Proof
     let proof = await client.submitProof(tree.getRoot(), 
@@ -204,7 +204,7 @@ app.post("/SewageComplaints", async (req, res) => {
     let tree = builder.build();
 
     // Creating a New Anchor Client
-    let client = anchor.connect(anchor.withCredentials("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhbmNob3IiLCJleHAiOjE3OTMyNzA0NDUsImp0aSI6ImFvNng3NWo1ZWVrcm0yNnpzY2sxeXdiaSIsInN1YiI6InV1aWd3Yzd6YXhlanJxZHNnYXF6M3NidSIsInNjb3BlIjoiMzEiLCJyb2xlIjoiRnJlZSJ9.Z--sFCzSKffue0cW4eyI4EuEM93R8bP8KRRUzqkGkhc"));
+    let client = anchor.connect(anchor.withCredentials(""));
 
     // Submitting Proof
     let proof = await client.submitProof(tree.getRoot(), 
